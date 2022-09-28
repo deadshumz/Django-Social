9a0b213bc3ae85524529bc0d56160735a696346c
Carlos Moerte <61364206+deadshumz@users.noreply.github.com>
2022-09-28 22:55:43 +0400

    Async get all users function

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from asgiref.sync import sync_to_async
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from .serializers import ( UserSerializer )
 
 # Create your views here.
+@sync_to_async
+@api_view(['GET'])
+def get_users(request):
+    queryset = User.objects.all()
+    serializer = UserSerializer(queryset, many=True)
+    return Response(serializer.data)

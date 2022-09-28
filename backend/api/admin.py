417822401a4c0b403e7a5cab09d3769b75ce60bb
Carlos Moerte <61364206+deadshumz@users.noreply.github.com>
2022-09-28 22:13:31 +0400

    correct name for user profile model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import Profile
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
     pass
 

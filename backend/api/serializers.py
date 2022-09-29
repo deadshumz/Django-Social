50dc39ab1b515b41772875ab8c274e2b7a02ad02
Carlos Moerte <61364206+deadshumz@users.noreply.github.com>
2022-09-29 19:58:25 +0400

    create user serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,4 +5,15 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        exclude = ("password",)
+        fields=('id', 'username', 'first_name', 'last_name', 'email', 'date_joined',)
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'], password=validated_data['password'])
+        return user
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,20 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from . import serializers
 
 
 @api_view(["GET"])
 def get_users(request):
     queryset = User.objects.all()
-    serializer = UserSerializer(queryset, many=True)
+    serializer = serializers.UserSerializer(queryset, many=True)
     return Response(serializer.data)
 
 
-# @api_view(['POST'])
-# def create_user(request):
+@api_view(['POST'])
+def create_user(request, fromat='json'):
+    serializer = serializers.CreateUserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        if user:
+            return Response(serializer.data)

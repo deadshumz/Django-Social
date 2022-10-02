c7b5aff105dbffbac09a8baf1b39afcdd0651f38
Carlos Moerte <61364206+deadshumz@users.noreply.github.com>
2022-10-02 23:56:03 +0400

    customuser likes field

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to="avatars", blank=True)
     bio = models.TextField(max_length=256, blank=True, default="default bio")
-    likes = models.ManyToManyField('self')
+    likes = models.ManyToManyField('Post', blank=True)
 
     def __str__(self):
         return self.username

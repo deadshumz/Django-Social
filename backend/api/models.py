1c9c3b0ceffc61ae35cff1896d85fdb5d1a3fc05
Carlos Moerte <61364206+deadshumz@users.noreply.github.com>
2022-10-02 23:27:56 +0400

    Post Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to="avatars", blank=True)
     bio = models.TextField(max_length=256, blank=True, default="default bio")
+    likes = models.ManyToManyField('self')
 
     def __str__(self):
         return self.username
+
+
+class Post(models.Model):
+    avatar = models.ImageField(upload_to="posts", blank=False)
+    description = models.TextField(max_length=256, blank=True)
